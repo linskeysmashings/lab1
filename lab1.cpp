f7b952eb54dc0181c873b5a1499754f7540aa23e
UbuntuVM <ooboontooz@Ubuntu-22-04-1.myguest.virtualbox.org>
2023-01-25 09:11:26 -0800

    my first commit for lab-1

---
+++ lab1.cpp
@@ -0,0 +1,14 @@
+//Linda Gamboa
+//lab-1 jan 24
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = rand() % 10 + 1;
+    cout << a << endl;
+    cout << "Hello World \n";
+
+    return 0;
+}
